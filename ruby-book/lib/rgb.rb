0147af7f65608216d0e77f7c2d4118c9dd52a842
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-01-09 18:06:02 +0900

    RGB処理をリファクタリング

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -5,10 +5,15 @@ def to_hex(r, g, b)
   # b.to_s(16).rjust(2, '0')
 
   # リファクタリング
-  hex = '#'
-  [r, g, b].each do |n|
-    hex += n.to_s(16).rjust(2, '0')
+  # hex = '#'
+  # [r, g, b].each do |n|
+  #   hex += n.to_s(16).rjust(2, '0')
+  # end
+  # hex
+
+  # さらにリファクタリング
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
   end
-  hex
 
 end
