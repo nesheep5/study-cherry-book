5116a95417b9182b92e7f0ea5ce04e37e0443683
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-01-10 18:17:40 +0900

    to_intsをリファクタリング

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -18,12 +18,37 @@ def to_hex(r, g, b)
 end
 
 def to_ints(hex)
-  r = hex[1..2]
-  g = hex[3..4]
-  b = hex[5..6]
-  ints = []
-  [r, g, b].each do |s|
-    ints << s.hex
-  end
-  ints
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # ints = []
+  # [r, g, b].each do |s|
+  #   ints << s.hex
+  # end
+  # ints
+
+  # リファクタリング
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  # リファクタリング
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  # リファクタリング
+  # r, g, b = hex.scan(/\w\w/)
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  # リファクタリング
+  hex.scan(/\w\w/).map(&:hex)
 end
