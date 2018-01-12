8ae5bddbf6512f7ffb0b93cc37cf15bdaff5871f
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-01-12 12:34:31 +0900

    キーワード引数に変更

--- ruby-book/test/convert_length_test.rb
+++ ruby-book/test/convert_length_test.rb
@@ -8,8 +8,13 @@ class ConvertLengthTest < Minitest::Test
     # assert_equal 10670.73, convert_length(35000, 'ft', 'm')
 
     # シンボルに変更
-    assert_equal 39.37, convert_length(1, :m, :in)
-    assert_equal 0.38, convert_length(15, :in, :m)
-    assert_equal 10670.73, convert_length(35000, :ft, :m)
+    # assert_equal 39.37, convert_length(1, :m, :in)
+    # assert_equal 0.38, convert_length(15, :in, :m)
+    # assert_equal 10670.73, convert_length(35000, :ft, :m)
+
+    # キーワード引数に変更
+    assert_equal 39.37, convert_length(1, from: :m, to: :in)
+    assert_equal 0.38, convert_length(15, from: :in, to: :m)
+    assert_equal 10670.73, convert_length(35000, from: :ft, to: :m)
   end
 end
