c2fbfc887d1e14ca2898792f22517217878bf7e5
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-01-12 12:29:39 +0900

    シンボルに変更

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,4 +1,10 @@
+# def convert_length(length, unit_from, unit_to)
+#   units = {'m' => 1.0, 'ft' => 3.28, 'in' => 39.37}
+#   (length / units[unit_from] * units[unit_to]).round(2)
+# end
+
+# シンボルに変更
+UNITS = {m: 1.0, ft: 3.28, in: 39.37}
 def convert_length(length, unit_from, unit_to)
-  units = {'m' => 1.0, 'ft' => 3.28, 'in' => 39.37}
-  (length / units[unit_from] * units[unit_to]).round(2)
+  (length / UNITS[unit_from] * UNITS[unit_to]).round(2)
 end
