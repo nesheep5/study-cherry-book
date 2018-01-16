da83af21cc0c36ebe7db9260d02c3153c6c2231b
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-01-16 17:25:23 +0900

    Gateテストを実装

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -1,9 +1,16 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
     # とりあえずGateオブジェクトが作れることを確認する
-    assert Gate.new
+    # assert Gate.new
+
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
