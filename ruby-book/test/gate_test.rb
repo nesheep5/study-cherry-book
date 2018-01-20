1797b4520c60cc2981efbdb26ad30dde7f34c284
shogo watanabe <ne.sheep.5mouths@gmail.com>
2018-01-21 07:50:35 +0900

    setupで作成したGateインスタンスを利用する要修正

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -11,22 +11,14 @@ class GateTest < Minitest::Test
   end
 
   def test_umeda_to_juso
-    # とりあえずGateオブジェクトが作れることを確認する
-    # assert Gate.new
-
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
   end
 
   def test_umeda_to_mikuni_when_fate_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
   end
 end
