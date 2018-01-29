77f902e3893f2d132c67f5f704d886f8e9e19b91
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-01-29 17:36:36 +0900

    ハッシュに対応

--- ruby-book/test/deep_freezable_test.rb
+++ ruby-book/test/deep_freezable_test.rb
@@ -11,4 +11,17 @@ class DeepFreezableTest < Minitest::Test
     # 配列の要素がすべてfreezeされているか？
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freezable_to_hash
+    # ハッシュの値は正しいか？
+    assert_equal(
+      {'Japan' => 'yen', 'US' => 'doller', 'India' => 'rupee'},
+      Bank::CURRENCIES
+    )
+    # ハッシュ自身がfreezeされているか？
+    assert Bank::CURRENCIES.frozen?
+    # ハッシュの要素がすべてfreezeされているか？
+    assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
+  end
+
 end
