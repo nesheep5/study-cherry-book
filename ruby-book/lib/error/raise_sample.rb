264d5ec3bac9ad3e271ff8d0da5869d4e63dc14a
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-02-01 17:23:12 +0900

    raiseの使い方を整理

---
+++ ruby-book/lib/error/raise_sample.rb
@@ -0,0 +1,17 @@
+# 例外を発生させる
+def currency_of(country)
+  case country
+  when :japan
+    'yen'
+  when :us
+    'dollar'
+  when :india
+    'rupee'
+  else
+    # 意図的に例外を発生させる
+    raise "向こうな国名です。#{country}"
+  end
+end
+
+currency_of(:japan)
+currency_of(:italy)
