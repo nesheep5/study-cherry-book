9d7c068bcab58725e066e1d62692592fb0379495
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2018-02-08 17:00:32 +0900

    test_echo修正

--- ruby-book/test/effects_test.rb
+++ ruby-book/test/effects_test.rb
@@ -10,6 +10,7 @@ class EffectsTest < Minitest::Test
   def test_echo
     effect = Effects.echo(2)
     assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!')
+    effect = Effects.echo(3)
     assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!')
   end
 end
